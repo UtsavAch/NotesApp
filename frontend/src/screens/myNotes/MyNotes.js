67f636564757b52da8aea766ff896e0337514ae5
UtsavAch <acharya.utsav.15@gmail.com>
2022-08-23 15:29:23 +0100

    Connecting backend and frontend

--- frontend/src/screens/myNotes/MyNotes.js
+++ frontend/src/screens/myNotes/MyNotes.js
@@ -1,75 +1,86 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Card, Badge } from "react-bootstrap";
-import MainScreen from "../../components/MainScreen";
-import notes from "../../data/notes";
+import { Button, Card, Badge, Accordion } from "react-bootstrap";
+import axios from "axios";
+import MainScreen from "../../components/MainScreen/MainScreen";
+import "./MyNotes.css";
 
 const MyNotes = () => {
+  const [notes, setNotes] = useState([]);
+
   const deleteHandler = (id) => {
     if (window.confirm("Are you sure?")) {
     }
   };
 
+  const fetchNotes = async () => {
+    const { data } = await axios.get("/api/notes/");
+    setNotes(data);
+  };
+
+  console.log(notes);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
   return (
     <MainScreen title="Welcome back Utsav...">
       <Link to="/createnote">
-        <Button
-          style={{
-            marginLeft: "10px",
-            marginBottom: "6px",
-            textTransform: "capitalize",
-          }}
-          size="lg"
-        >
+        <Button className="mainscreenButton" size="lg">
           New note
         </Button>
       </Link>
 
-      {notes.map((note) => (
-        <Card style={{ margin: 10 }} key={note._id}>
-          <Card.Header style={{ display: "flex" }}>
-            <span
-              style={{
-                color: "#444",
-                textDecoration: "none",
-                flex: 1,
-                cursor: "pointer",
-                alignSelf: "center",
-                fontSize: 24,
-                fontWeight: "bold",
-              }}
-            >
-              {note.title}
-            </span>
-            <div>
-              <Link to={`/note/${note._id}`}>
-                <Button>Edit</Button>
-              </Link>
-              <Button
-                variant="danger"
-                className="mx-2"
-                onClick={() => {
-                  deleteHandler(note._id);
-                }}
-              >
-                Delete
-              </Button>
-            </div>
-          </Card.Header>
+      <Accordion defaultActiveKey="0">
+        {notes.map((note) => (
+          <Accordion.Item
+            eventKey={`${notes.indexOf(note)}`}
+            key={note._id}
+            className="accordionItem"
+          >
+            <Card className="noteCard">
+              <Accordion.Header className="accordionHeader">
+                <Card.Header className="noteHeader">
+                  <span className="noteTitle">{note.title}</span>
+                  <div className="noteButtonContainer">
+                    <Link
+                      to={`/note/${note._id}`}
+                      className="noteButton noteEditButton"
+                    >
+                      Edit
+                    </Link>
+                    <div
+                      className="noteButton noteDeleteButton"
+                      onClick={() => {
+                        deleteHandler(note._id);
+                      }}
+                    >
+                      Delete
+                    </div>
+                  </div>
+                </Card.Header>
+              </Accordion.Header>
 
-          <Card.Body>
-            <h4>
-              <Badge bg="light" text="dark">
-                Category - {note.category}
-              </Badge>
-            </h4>
-            <blockquote className="blockquote mb-0">
-              <p>{note.content}</p>
-              <footer className="blockquote-footer">Created on -Date</footer>
-            </blockquote>
-          </Card.Body>
-        </Card>
-      ))}
+              <Accordion.Body>
+                <Card.Body className="noteBody">
+                  <h4>
+                    <Badge bg="light" text="dark">
+                      Category - {note.category}
+                    </Badge>
+                  </h4>
+                  <blockquote className="blockquote mb-0">
+                    <p>{note.content}</p>
+                    <footer className="blockquote-footer">
+                      Created on -Date
+                    </footer>
+                  </blockquote>
+                </Card.Body>
+              </Accordion.Body>
+            </Card>
+          </Accordion.Item>
+        ))}
+      </Accordion>
     </MainScreen>
   );
 };
