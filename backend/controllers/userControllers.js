0eda6861051c99ed09c321b10de7312e12265c39
UtsavAch <acharya.utsav.15@gmail.com>
2022-08-24 18:33:38 +0100

    Login functionality and authentication

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const generateToken = require("../utils/generateToken");
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, isAdmin, pic } = req.body;
@@ -14,11 +15,38 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create({ name, email, password, isAdmin, pic });
 
   if (user) {
-    res.status(201).json(user);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
   } else {
     res.status(400);
     throw new Error("Error occured!");
   }
 });
 
-module.exports = { registerUser };
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid email or password!");
+  }
+});
+
+module.exports = { registerUser, authUser };
